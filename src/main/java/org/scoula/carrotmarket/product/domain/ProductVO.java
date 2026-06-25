1763b56d2f80fd21159305e07e49ab67d8af1950
cow9708-png <cow9708@gmail.com>
2026-06-25 17:36:52 +0900

    수ì©정해뜬

--- src/main/java/org/scoula/carrotmarket/product/domain/ProductVO.java
+++ src/main/java/org/scoula/carrotmarket/product/domain/ProductVO.java
@@ -1,4 +1,27 @@
 package org.scoula.carrotmarket.product.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductVO {
+    private Long productId;
+    private Long userId;
+
+    private String title;
+    private Integer price;
+    private String imageUrl;
+    private String category;
+    private String description;
+    private String status;
+
+    private LocalDateTime createdAt;
 }
--- src/main/java/org/scoula/carrotmarket/product/dto/ProductDTO.java
+++ src/main/java/org/scoula/carrotmarket/product/dto/ProductDTO.java
@@ -1,15 +1,58 @@
 package org.scoula.carrotmarket.product.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.scoula.carrotmarket.product.domain.ProductVO;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductDTO {
+
     private Long productId;
     private Long userId;
+
     private String title;
-    private int price;
+    private Integer price;
     private String imageUrl;
     private String category;
-    private Long Description;
+    private String description;
     private String status;
-    private LocalDateTime createAt;
+
+    private LocalDateTime createdAt;
+
+    // DTO -> VO
+    public ProductVO toVo() {
+        return ProductVO.builder()
+                .productId(productId)
+                .userId(userId)
+                .title(title)
+                .price(price)
+                .imageUrl(imageUrl)
+                .category(category)
+                .description(description)
+                .status(status)
+                .createdAt(createdAt)
+                .build();
+    }
+
+    // VO -> DTO
+    public static ProductDTO of(ProductVO vo) {
+        return vo == null ? null : ProductDTO.builder()
+                .productId(vo.getProductId())
+                .userId(vo.getUserId())
+                .title(vo.getTitle())
+                .price(vo.getPrice())
+                .imageUrl(vo.getImageUrl())
+                .category(vo.getCategory())
+                .description(vo.getDescription())
+                .status(vo.getStatus())
+                .createdAt(vo.getCreatedAt())
+                .build();
+    }
 }
