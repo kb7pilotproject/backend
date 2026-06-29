4891250ef8fe008137a1ba020534178804686025
soobin-shin <tlstnqls6257@gmail.com>
2026-06-29 17:12:14 +0900

    feat: 로그인/로그아웃 기능 구현

--- src/main/java/org/scoula/carrotmarket/user/mapper/UserMapper.java
+++ src/main/java/org/scoula/carrotmarket/user/mapper/UserMapper.java
@@ -1,4 +1,11 @@
 package org.scoula.carrotmarket.user.mapper;
 
-public class UserMapper {
+import org.apache.ibatis.annotations.Select;
+import org.scoula.carrotmarket.user.domain.UserVO;
+
+public interface UserMapper {
+    @Select("SELECT user_id as userId, username, password, email " +
+            "FROM user WHERE username = #{username}")
+    UserVO findByUsername(String username);
+
 }
--- src/main/java/org/scoula/carrotmarket/user/service/UserService.java
+++ src/main/java/org/scoula/carrotmarket/user/service/UserService.java
@@ -1,4 +1,8 @@
 package org.scoula.carrotmarket.user.service;
 
+import org.scoula.carrotmarket.user.dto.UserDTO;
+
 public interface UserService {
+    // 로그인: 성공하면 회원정보(DTO) 반환, 실패하면 null
+    UserDTO login(String username, String password);
 }
