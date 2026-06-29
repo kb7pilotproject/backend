be06ca053f7a8620bd360012b098e99c9855421e
yxngbxyxn1003 <boyunyang@naver.com>
2026-06-29 17:46:34 +0900

    init the controller

--- src/main/java/org/scoula/carrotmarket/comment/controller/CommentController.java
+++ src/main/java/org/scoula/carrotmarket/comment/controller/CommentController.java
@@ -1,5 +1,15 @@
 package org.scoula.carrotmarket.comment.controller;
 
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comment")
+@RequiredArgsConstructor
+@Log4j2
+
 public class CommentController {
 }
