c179377f868aa01f4af9ccb631ef3bb613512454
yxngbxyxn1003 <boyunyang@naver.com>
2026-06-29 17:28:14 +0900

    add ServiceImpl

--- src/main/java/org/scoula/carrotmarket/comment/service/CommentServiceImpl.java
+++ src/main/java/org/scoula/carrotmarket/comment/service/CommentServiceImpl.java
@@ -1,51 +1,47 @@
 package org.scoula.carrotmarket.comment.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.scoula.carrotmarket.comment.domain.CommentVO;
 import org.scoula.carrotmarket.comment.dto.CommentDTO;
 import org.scoula.carrotmarket.comment.mapper.CommentMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-@Log4j2
 public class CommentServiceImpl implements CommentService {
-
 
     private final CommentMapper commentMapper;
 
     @Override
     public void insert(CommentDTO comment) {
-
+        commentMapper.insert(comment.toVo());
     }
 
     @Override
     public CommentVO select(int commentId) {
-        return null;
+        return commentMapper.select(commentId);
     }
 
     @Override
     public List<CommentVO> selectByProductId(int productId) {
-        return List.of();
+        return commentMapper.selectByProductId(productId);
     }
 
     @Override
     public void update(CommentDTO comment) {
-        CommentVO vo = comment.toVo();
         commentMapper.update(comment.toVo());
     }
 
     @Override
     public void delete(CommentDTO comment) {
-        CommentVO vo = comment.toVo();
         commentMapper.delete(comment.toVo());
     }
 
     @Override
     public int countByProductId(int productId) {
-        return 0;
+        return commentMapper.countByProductId(productId);
     }
+
 }
